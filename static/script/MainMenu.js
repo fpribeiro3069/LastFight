27fc8c2042cb5125004c361343ad3b640e407bf8
Francisco Ribeiro <franciscoribeiro24@gmail.com>
2019-04-14 14:12:35 +0100

    Alguns fixes ao main menu Barra loading ainda não aparece

--- static/script/MainMenu.js
+++ static/script/MainMenu.js
@@ -11,22 +11,25 @@ class MainMenu extends Phaser.Scene {
     gameState.menu.creditos = this.add.sprite(380, 400, 'creditos').setOrigin(0, 0);
     gameState.menu.somIcon = this.add.sprite(20, 530, 'som').setOrigin(0, 0);
     gameState.menu.som = this.sound.add('menuSound', {loop: true})
-
     gameState.menu.somIcon.setInteractive({useHandCursor: true});
-    gameState.menu.somIcon.setFrame(1)
+    gameState.menu.somIcon.setFrame(0)
     gameState.menu.somIcon.on('pointerdown', function() {
       if(gameState.sound) {
         this.setFrame(0)
         // Turn off sound
-        gameState.menu.som.stop()
+        gameState.menu.som.mute = true
       } else {
         this.setFrame(1)
         // Turn on sound
-        gameState.menu.som.play()
+        if(gameState.menu.som.mute) {
+          gameState.menu.som.mute = false
+        } else
+          gameState.menu.som.play()
       }
       gameState.sound = !gameState.sound
     })
 
+    // Mudar as frames dos botoes em pointerover
     let buttons = this.getMainMenuButtons();
     for(let button of buttons) {
       button.setInteractive({useHandCursor: true})
@@ -37,6 +40,22 @@ class MainMenu extends Phaser.Scene {
         button.setFrame(0)
       })
     }
+
+    function animateButtons() {
+      let buttons = this.getMainMenuButtons();
+      for (let button of buttons) {
+        button.y += gameState.buttonPos;
+      }
+      gameState.buttonPos *= -1
+    }
+
+    gameState.buttonPos = -3
+    this.time.addEvent({
+      delay: 250,
+      callback: animateButtons,
+      callbackScope: this,
+      loop: true
+    })
   }
 
   getMainMenuButtons() {
